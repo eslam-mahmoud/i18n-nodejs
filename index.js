8af7ab0f072bba0a0c0830532211f08eef402620
Eslam Mahmoud <mail.eslammahmoud@gmail.com>
2024-01-18 09:42:17 -0600

    updating lang usage

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ class I18n {
 	//Get the rule for pluralization
 	//http://localization-guide.readthedocs.org/en/latest/l10n/pluralforms.html 
 	get_rule(count, language) {
-		switch(language){
+		switch (language) {
 			//nplurals=2; plural=(n > 1);
 			case 'ach':
 			case 'ak':
@@ -29,7 +29,7 @@ class I18n {
 			case 'wa':
 				return (count > 1) ? 1 : 0;
 				break;
-			
+
 			//nplurals=2; plural=(n != 1);
 			case 'af':
 			case 'an':
@@ -140,12 +140,12 @@ class I18n {
 
 			//nplurals=2; plural=(n%10!=1 || n%100==11);
 			case 'is':
-				return (count%10!=1 || count%100==11) ? 1 : 0;
+				return (count % 10 != 1 || count % 100 == 11) ? 1 : 0;
 				break;
 
 			//nplurals=4; plural=(n==1) ? 0 : (n==2) ? 1 : (n == 3) ? 2 : 3;
 			case 'kw':
-				return (count==1) ? 0 : (count==2) ? 1 : (count == 3) ? 2 : 3;
+				return (count == 1) ? 0 : (count == 2) ? 1 : (count == 3) ? 2 : 3;
 				break;
 
 			//nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);
@@ -155,82 +155,82 @@ class I18n {
 			case 'hr':
 			case 'bs':
 			case 'be':
-				return count%10==1 && count%100!=11 ? 0 : count%10>=2 && count%10<=4 && (count%100<10 || count%100>=20) ? 1 : 2;
+				return count % 10 == 1 && count % 100 != 11 ? 0 : count % 10 >= 2 && count % 10 <= 4 && (count % 100 < 10 || count % 100 >= 20) ? 1 : 2;
 				break;
 
 			//nplurals=3; plural=(n==0 ? 0 : n==1 ? 1 : 2);
 			case 'mnk':
-				return count==0 ? 0 : count==1 ? 1 : 2;
+				return count == 0 ? 0 : count == 1 ? 1 : 2;
 				break;
 
 			//nplurals=3; plural=(n==1) ? 0 : (n>=2 && n<=4) ? 1 : 2;
 			case 'sk':
-				return (count==1) ? 0 : (count>=2 && count<=4) ? 1 : 2;
+				return (count == 1) ? 0 : (count >= 2 && count <= 4) ? 1 : 2;
 				break;
 
 			//nplurals=3; plural=(n==1 ? 0 : (n==0 || (n%100 > 0 && n%100 < 20)) ? 1 : 2);
 			case 'ro':
-				return count==1 ? 0 : (count==0 || (count%100 > 0 && count%100 < 20)) ? 1 : 2;
+				return count == 1 ? 0 : (count == 0 || (count % 100 > 0 && count % 100 < 20)) ? 1 : 2;
 				break;
 
 			//nplurals=6; plural=(n==0 ? 0 : n==1 ? 1 : n==2 ? 2 : n%100>=3 && n%100<=10 ? 3 : n%100>=11 ? 4 : 5);
 			case 'ar':
-				return count==0 ? 0 : count==1 ? 1 : count==2 ? 2 : count%100>=3 && count%100<=10 ? 3 : count%100>=11 ? 4 : 5;
+				return count == 0 ? 0 : count == 1 ? 1 : count == 2 ? 2 : count % 100 >= 3 && count % 100 <= 10 ? 3 : count % 100 >= 11 ? 4 : 5;
 				break;
 
 			//nplurals=3; plural=(n==1) ? 0 : (n>=2 && n<=4) ? 1 : 2;
 			case 'cs':
-				return count==1 ? 0 : (count>=2 && count<=4) ? 1 : 2;
+				return count == 1 ? 0 : (count >= 2 && count <= 4) ? 1 : 2;
 				break;
 
 			//countplurals=3; plural=(n==1) ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2;
 			case 'csb':
-				return (count==1) ? 0 : count%10>=2 && count%10<=4 && (count%100<10 || count%100>=20) ? 1 : 2;
+				return (count == 1) ? 0 : count % 10 >= 2 && count % 10 <= 4 && (count % 100 < 10 || count % 100 >= 20) ? 1 : 2;
 				break;
 
 			//nplurals=4; plural=(n==1) ? 0 : (n==2) ? 1 : (n != 8 && n != 11) ? 2 : 3;
 			case 'cy':
-				return (count==1) ? 0 : (count==2) ? 1 : (count != 8 && count != 11) ? 2 : 3;  
+				return (count == 1) ? 0 : (count == 2) ? 1 : (count != 8 && count != 11) ? 2 : 3;
 				break;
 
 			//nplurals=5; plural=n==1 ? 0 : n==2 ? 1 : (n>2 && n<7) ? 2 :(n>6 && n<11) ? 3 : 4;
 			case 'ga':
-				return count==1 ? 0 : count==2 ? 1 : (count>2 && count<7) ? 2 :(count>6 && count<11) ? 3 : 4;
+				return count == 1 ? 0 : count == 2 ? 1 : (count > 2 && count < 7) ? 2 : (count > 6 && count < 11) ? 3 : 4;
 				break;
 
 			//nplurals=4; plural=(n==1 || n==11) ? 0 : (n==2 || n==12) ? 1 : (n > 2 && n < 20) ? 2 : 3;
 			case 'gd':
-				return (count==1 || count==11) ? 0 : (count==2 || count==12) ? 1 : (count > 2 && count < 20) ? 2 : 3; 
+				return (count == 1 || count == 11) ? 0 : (count == 2 || count == 12) ? 1 : (count > 2 && count < 20) ? 2 : 3;
 				break;
 
 			//nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && (n%100<10 || n%100>=20) ? 1 : 2);
 			case 'it':
-				return count%10==1 && count%100!=11 ? 0 : count%10>=2 && (count%100<10 || count%100>=20) ? 1 : 2; 
+				return count % 10 == 1 && count % 100 != 11 ? 0 : count % 10 >= 2 && (count % 100 < 10 || count % 100 >= 20) ? 1 : 2;
 				break;
 
 			//nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n != 0 ? 1 : 2);
 			case 'lv':
-				return count%10==1 && count%100!=11 ? 0 : count != 0 ? 1 : 2; 
+				return count % 10 == 1 && count % 100 != 11 ? 0 : count != 0 ? 1 : 2;
 				break;
 
 			//nplurals=2; plural= n==1 || n%10==1 ? 0 : 1;
 			case 'mk':
-				return count=2; plural= count==1 || count%10==1 ? 0 : 1;
+				return count = 2; plural = count == 1 || count % 10 == 1 ? 0 : 1;
 				break;
 
 			//nplurals=4; plural=(n==1 ? 0 : n==0 || ( n%100>1 && n%100<11) ? 1 : (n%100>10 && n%100<20 ) ? 2 : 3);
 			case 'mt':
-				return count==1 ? 0 : count==0 || (count%100>1 && n%100<11) ? 1 : (count%100>10 && count%100<20 ) ? 2 : 3;
+				return count == 1 ? 0 : count == 0 || (count % 100 > 1 && n % 100 < 11) ? 1 : (count % 100 > 10 && count % 100 < 20) ? 2 : 3;
 				break;
 
 			//nplurals=3; plural=(n==1 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);
 			case 'pl':
-				return count==1 ? 0 : count%10>=2 && count%10<=4 && (count%100<10 || count%100>=20) ? 1 : 2;
+				return count == 1 ? 0 : count % 10 >= 2 && count % 10 <= 4 && (count % 100 < 10 || count % 100 >= 20) ? 1 : 2;
 				break;
 
 			//nplurals=4; plural=(n%100==1 ? 1 : n%100==2 ? 2 : n%100==3 || n%100==4 ? 3 : 0);
 			case 'sl':
-				return count%100==1 ? 1 : count%100==2 ? 2 : count%100==3 || count%100==4 ? 3 : 0;
+				return count % 100 == 1 ? 1 : count % 100 == 2 ? 2 : count % 100 == 3 || count % 100 == 4 ? 3 : 0;
 				break;
 
 			default:
@@ -238,9 +238,15 @@ class I18n {
 		}
 	}
 
-	__(string, values) {
+	__(string, values, lang) {
+		//if lang is passed then use it, else use the default lang
+		if (!lang) {
+			lang = this._lang;
+		}
+
 		//return translation of the original sting if did not find the translation
 		var translation = string;
+
 		//get the corresponding translation from the file
 		if (typeof this._locale[string] != "undefined" && typeof this._locale[string][this._lang] != "undefined") {
 			translation = this._locale[string][this._lang];
@@ -258,7 +264,7 @@ class I18n {
 				var match_word = (match.replace('}}', '')).replace('{{', '');
 
 				//translate the word if was passed in the values var
-				if (values && typeof values[match_word] != "undefined" ) {
+				if (values && typeof values[match_word] != "undefined") {
 					translation = translation.replace(match, values[match_word]);
 					continue;//move to the next word in the loop
 				} else if (typeof this._locale[match_word] != "undefined" && typeof this._locale[match_word][this._lang] != "undefined") {
